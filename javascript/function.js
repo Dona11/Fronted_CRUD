8f7ceb810b6e6fab8bf236e5b4dc820eaae41a83
lai2-01 <lai2-01@itismeucci.com>
2022-02-14 11:46:06 +0100

    primo commit

---
+++ javascript/function.js
@@ -0,0 +1,44 @@
+var data = [
+    {
+      "id": 10001,
+      "birthDate": "1953-09-01",
+      "firstName": "Georgi",
+      "lastName": "Facello",
+      "gender": "M",
+      "hireDate": "1986-06-25",
+    },
+    {
+      "id": 10002,
+      "birthDate": "1964-06-01",
+      "firstName": "Bezalel",
+      "lastName": "Simmel",
+      "gender": "F",
+      "hireDate": "1985-11-20",
+    },
+    {
+      "id": 10003,
+      "birthDate": "1959-12-02",
+      "firstName": "Parto",
+      "lastName": "Bamford",
+      "gender": "M",
+      "hireDate": "1986-08-27T22:00:00.000+0000",
+    },
+    {
+      "id": 10004,
+      "birthDate": "1954-04-30",
+      "firstName": "Chirstian",
+      "lastName": "Koblick",
+      "gender": "M",
+      "hireDate": "1986-11-30",
+  
+    },
+    {
+      "id": 10005,
+      "birthDate": "1955-01-20",
+      "firstName": "Kyoichi",
+      "lastName": "Maliniak",
+      "gender": "M",
+      "hireDate": "1989-09-11T22:00:00.000+0000",
+  
+    }
+  ];
