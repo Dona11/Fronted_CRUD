3b72e2101f4b8c621a67199a1c1f2fe40c89a174
Dona11 <matti.dona03@gmail.com>
2022-02-20 21:19:10 +0100

    secondo commit

--- javascript/function.js
+++ javascript/function.js
@@ -5,7 +5,6 @@ var data = [
       "firstName": "Georgi",
       "lastName": "Facello",
       "gender": "M",
-      "hireDate": "1986-06-25",
     },
     {
       "id": 10002,
@@ -13,7 +12,6 @@ var data = [
       "firstName": "Bezalel",
       "lastName": "Simmel",
       "gender": "F",
-      "hireDate": "1985-11-20",
     },
     {
       "id": 10003,
@@ -21,7 +19,6 @@ var data = [
       "firstName": "Parto",
       "lastName": "Bamford",
       "gender": "M",
-      "hireDate": "1986-08-27T22:00:00.000+0000",
     },
     {
       "id": 10004,
@@ -29,7 +26,6 @@ var data = [
       "firstName": "Chirstian",
       "lastName": "Koblick",
       "gender": "M",
-      "hireDate": "1986-11-30",
   
     },
     {
@@ -38,7 +34,87 @@ var data = [
       "firstName": "Kyoichi",
       "lastName": "Maliniak",
       "gender": "M",
-      "hireDate": "1989-09-11T22:00:00.000+0000",
   
     }
   ];
+
+  var nextId = 100;
+
+  $(document).ready(function(){
+
+    displayEmployeeList();
+
+    //creare un nuovo impiegato
+    $('#create-employee-form').on('submit', function(e){
+      e.preventDefault();
+      var form_action = $("#create-employee-form").attr("action");
+      var date = $("#date").val();
+      var name = $("#name").val();
+      var surname = $("#surname").val();
+      var sex = $("#sex").val();
+      display = "create";
+
+      if(name != '' && surname != '' && date != '' && sex != ''){
+        data.push({id: nextId, birthDate: date, firstName: name, lastName: surname, gender: sex});
+        nextId++;
+
+        displayEmployeeList();
+
+        $("#create-employee").modal('hide');
+        toastr.success('Impiegato Creato Correttamente.', 'Successo', {timeOut: 5000});
+
+      }else{
+        alert('Tutti i campi sono obbligatori. Assicurati di compilare tutti i campi correttamente.')
+      }
+    });
+
+    //visualizzare la lista degli impiegati
+    function displayEmployeeList(){
+      var rows = '';
+      $.each(data, function(index, value){
+        rows = rows + '<tr>';
+        rows = rows + '<td>' + value.id + '</td>';
+        rows = rows + '<td>' + value.birthDate + '</td>';
+        rows = rows + '<td>' + value.firstName + '</td>';
+        rows = rows + '<td>' + value.lastName + '</td>';
+        rows = rows + '<td>' + value.gender + '</td>';
+        rows = rows + '<td data-id="'+value.id+'">';
+        rows = rows + '<button class="btn btn-danger btn-sm delete-employee">Elimina</button>';
+        rows = rows + '&nbsp&nbsp';
+        rows = rows + '<button class="btn btn-secondary btn-sm edit-employee">Modifica</button>';
+        rows = rows + '</td>';
+        rows = rows + '</tr>';
+      });
+      $("tbody").html(rows);
+    }
+
+    //modificare un impiegato
+    $("body").on("click","edit-employee-form",function(){
+      var id = $(this).parent("td").data('id');
+      var date = $(this).parent("td").data('id').text();
+      var name = $(this).parent("td").data('id').text();
+      var surname = $(this).parent("td").data('id').text();
+      var sex = $(this).parent("td").data('id').text();
+      display = "edit";
+
+      $("#date_edit").val(date);
+      $("#name_edit").val(name);
+      $("#surname_edit").val(surname);
+      $("#sex_edit").val(sex);
+      $("#edit-employee-form").find(".edit-id").val(id);
+      
+    });
+
+    //eliminare un impiegato
+    $("boby").on("click",".delete-employee",function(){
+      var id = $(this).parent("td").data('id');
+      for(let i = 0; i < data.length; i++){
+        if(data[i].id == id){
+          data.splice(i, 1);
+        break;
+        }
+      }
+      displayEmployeeList();
+    });
+
+  });
